dbe59e9ceed3f1589f215f6622fd3fba7070f02d
Chris Romero <chris.romero@padillaco.com>
2022-08-11 22:44:08 -0400

    more fixes

--- netlify/functions/metadata/browser.js
+++ netlify/functions/metadata/browser.js
@@ -1,13 +1,14 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 
 async function startBrowser() {
   let browser;
   try {
     console.log("Opening the browser......");
     browser = await puppeteer.launch({
-      headless: true,
-      args: ["--disable-setuid-sandbox"],
-      'ignoreHTTPSErrors': true
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: true
     });
   } catch (err) {
     console.log("Could not create a browser instance => : ", err);
